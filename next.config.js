0b9f7643c2439ca217d0229e867ea3c9aa10d1b5
Simranjeet Singh <13simran37@gmail.com>
2023-04-30 19:49:06 -0400

    added next-pws thingy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
+const nextDataIndex = runtimeCaching.findIndex(
+  (entry) => entry.options.cacheName === "next-data"
+);
+
+if (nextDataIndex !== -1) {
+  runtimeCaching[nextDataIndex].handler = "NetworkFirst";
+} else {
+  throw new Error("Failed to find next-data object in runtime caching");
+}
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  runtimeCaching,
+  register: true,
+  skipWaiting: true,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === 'development'
+});
+
 const nextConfig = {
   experimental: {
     appDir: true,
@@ -8,4 +28,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
