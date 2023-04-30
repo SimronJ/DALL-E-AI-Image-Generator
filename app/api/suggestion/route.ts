2b6dacf9bbce5b35dde6a33863ab1d8bddb78f1f
Simranjeet Singh <13simran37@gmail.com>
2023-04-30 19:18:43 -0400

    added time to the api/suggested

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -2,10 +2,10 @@
 export async function GET(request: Request) {
   const isLocalhost = process.env.NODE_ENV === 'development';
   const baseUrl = isLocalhost ? 'http://localhost:7071' : 'https://ron-ai-image-generator-app.azurewebsites.net';
-  const apiUrl = `${baseUrl}/api/getChatGPTSuggestion`;
+  const apiUrl = `${baseUrl}/api/getChatGPTSuggestion?timestamp=${new Date().getTime()}`;
   console.log(apiUrl);
 
-  const response = await fetch("https://ron-ai-image-generator-app.azurewebsites.net/api/getChatGPTSuggestion", {
+  const response = await fetch(apiUrl, {
     cache: 'no-store'
   });
 
--- lib/fetchSuggestionFromChatGPT.ts
+++ lib/fetchSuggestionFromChatGPT.ts
@@ -1,7 +1,9 @@
+console.log("getting suggestion");
 const fetchSuggestionFromChatGPT = () =>
     fetch("/api/suggestion", {
         cache: 'no-store'
     }).then( (res) => res.json());
 
+    console.log("got suggestion");
 
 export default fetchSuggestionFromChatGPT;
